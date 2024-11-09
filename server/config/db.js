bd937df7ec288d041562dee46f5bd8348bb91dae
palak_jaiswal <120741420+Palak4620@users.noreply.github.com>
2024-11-09 10:15:58 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
+    console.log(process.env.MONGO_URI);
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
