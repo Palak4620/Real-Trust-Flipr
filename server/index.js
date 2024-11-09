b47a44bcdc85771a6ac53978a00e0d4a49114c7e
palak_jaiswal <120741420+Palak4620@users.noreply.github.com>
2024-11-09 09:40:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
 const express = require('express');
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const cors = require('cors');
-
 require('dotenv').config();
 
 const app = express();
 const path = require('path');
 
+// Connect to MongoDB
 connectDB();
 
-app.use(cors(
-  {
-    origin: ["http://real-trust-flipr.vercelapp"],
-    method: ["POST", "GET"],
-    credentials: true  
-  }
-));
-app.use(express.json());
-mongoose.connect('mongodb+srv://palak_jaiswal:Palak0608>@cluster0.oumg4.mongodb.net/flipr?retryWrites=true&w=majority&appName=Cluster0');
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// CORS configuration
+app.use(cors({
+  origin: ["http://real-trust-flipr.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true  
+}));
 
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/projects', require('./routes/projectRoutes'));
@@ -28,5 +26,6 @@ app.use('/api/clients', require('./routes/clientRoutes'));
 app.use('/api/contact', require('./routes/contactRoutes'));
 app.use('/api/subscriptions', require('./routes/subscriptionRoutes'));
 
+// Server setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
