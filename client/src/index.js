9a78f4c915bd0c23e965c130a88ce91657b7ba65
palak_jaiswal <120741420+Palak4620@users.noreply.github.com>
2024-11-09 22:52:49 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <ErrorBoundary>
         <App />
-      </ErrorBoundary>
     </Router>
   </React.StrictMode>
 );
