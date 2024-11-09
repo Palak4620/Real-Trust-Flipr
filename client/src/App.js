0109f935c70bf4e148f2ee492d611622544e11e8
palak_jaiswal <120741420+Palak4620@users.noreply.github.com>
2024-11-10 00:06:44 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AdminPanel from './pages/AdminPanel';
 
@@ -9,6 +9,7 @@ function App() {
            <Routes>
                <Route path="/" element={<LandingPage />} />
                <Route path="/admin" element={<AdminPanel />} />
+               <Route path="*" element={<Navigate to="/admin" replace />} />
            </Routes>
        </Router>
    );
